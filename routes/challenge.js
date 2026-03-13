705eaa5bf75fa480c1292c67c614fd7e68194bb9
SUHEKU831 <suheku83@gmail.com>
2026-03-13 14:52:23 +0800

    Update challenge.js

--- routes/challenge.js
+++ routes/challenge.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const crypto = require("crypto")
 const db = require("../database")
+const auth = require("../middleware/auth")
 
 const router = express.Router()
 
-router.get("/",(req,res)=>{
+router.get("/",auth,(req,res)=>{
 
 db.all("SELECT * FROM challenges",(err,challs)=>{
 
@@ -14,15 +15,27 @@ res.render("challenge",{challs})
 
 })
 
-router.post("/submit",(req,res)=>{
+router.post("/submit",auth,(req,res)=>{
 
 const {flag,id} = req.body
 
+const hash = crypto
+.createHash("sha256")
+.update(flag)
+.digest("hex")
+
 db.get("SELECT * FROM challenges WHERE id=?",[id],(err,chall)=>{
 
-if(!chall) return res.send("Invalid")
+if(!chall) return res.send("Invalid challenge")
 
-if(flag === chall.flag){
+if(hash === chall.flag_hash){
+
+db.get(
+"SELECT * FROM solves WHERE user_id=? AND challenge_id=?",
+[req.session.user.id,id],
+(err,row)=>{
+
+if(row) return res.send("Already solved")
 
 db.run(
 "INSERT INTO solves(user_id,challenge_id) VALUES(?,?)",
@@ -34,10 +47,15 @@ db.run(
 [chall.points,req.session.user.id]
 )
 
-return res.send("Correct flag")
-}
+res.send("Correct flag")
+
+})
+
+}else{
 
 res.send("Wrong flag")
+
+}
 
 })
 
