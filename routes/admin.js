80a99645be7aa235e87c1174d3da61cd9ca875ff
SUHEKU831 <suheku83@gmail.com>
2026-03-13 14:52:39 +0800

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const crypto = require("crypto")
 const db = require("../database")
+const admin = require("../middleware/admin")
 
 const router = express.Router()
 
-router.get("/",(req,res)=>{
+router.get("/",admin,(req,res)=>{
 
 db.all("SELECT * FROM challenges",(err,challs)=>{
 
@@ -13,13 +15,18 @@ res.render("admin",{challs})
 
 })
 
-router.post("/add",(req,res)=>{
+router.post("/add",admin,(req,res)=>{
 
 const {title,description,flag,points,category} = req.body
 
+const hash = crypto
+.createHash("sha256")
+.update(flag)
+.digest("hex")
+
 db.run(
-"INSERT INTO challenges(title,description,flag,points,category) VALUES(?,?,?,?,?)",
-[title,description,flag,points,category]
+"INSERT INTO challenges(title,description,flag_hash,points,category) VALUES(?,?,?,?,?)",
+[title,description,hash,points,category]
 )
 
 res.redirect("/admin")
