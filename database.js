3e0cdf32a7831457ff827d88ac4a07afd1549a65
SUHEKU831 <suheku83@gmail.com>
2026-03-13 14:40:15 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,38 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = new sqlite3.Database("./database.db")
+
+db.serialize(()=>{
+
+db.run(`
+CREATE TABLE IF NOT EXISTS users(
+id INTEGER PRIMARY KEY,
+username TEXT UNIQUE,
+password TEXT,
+score INTEGER DEFAULT 0,
+isAdmin INTEGER DEFAULT 0
+)
+`)
+
+db.run(`
+CREATE TABLE IF NOT EXISTS challenges(
+id INTEGER PRIMARY KEY,
+title TEXT,
+description TEXT,
+flag TEXT,
+points INTEGER,
+category TEXT
+)
+`)
+
+db.run(`
+CREATE TABLE IF NOT EXISTS solves(
+id INTEGER PRIMARY KEY,
+user_id INTEGER,
+challenge_id INTEGER
+)
+`)
+
+})
+
+module.exports = db
