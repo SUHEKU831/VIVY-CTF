c0f1c27714ec5e9f5519abd0b4c606a1da44f1d6
SUHEKU831 <suheku83@gmail.com>
2026-03-13 20:37:25 +0800

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,11 @@
-module.exports = function(req,res,next){
-
-if(!req.session.user){
-return res.redirect("/login")
-}
-
-next()
-
+// middleware/auth.js
+module.exports = function(req, res, next) {
+    // Cek apakah user sudah login
+    if (!req.session || !req.session.user) {
+        // Redirect ke halaman login jika belum login
+        return res.redirect("/auth/login");
+    }
+    
+    // Lanjut ke route berikutnya jika sudah login
+    next();
 }
