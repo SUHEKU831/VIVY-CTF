1c946bc8a7aedd42be0c8ca148372708a333734c
SUHEKU831 <suheku83@gmail.com>
2026-03-25 07:21:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.use((err, req, res, next) => {
 })
 
 // ================= START SERVER =================
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("🚀 CTF running on http://localhost:" + PORT)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT)
 })
