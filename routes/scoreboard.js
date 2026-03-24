52bafd6a9c4785ba7c3a560c64217d9674efc296
SUHEKU831 <suheku83@gmail.com>
2026-03-25 07:17:39 +0800

    Update scoreboard.js

--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -5,11 +5,17 @@ const router = express.Router()
 
 router.get("/",(req,res)=>{
 
-db.all(
-"SELECT username,score FROM users ORDER BY score DESC",
-(err,users)=>{
+db.all(`
+SELECT 
+    teams.name as team_name,
+    COALESCE(SUM(users.score),0) as score
+FROM teams
+LEFT JOIN users ON users.team_id = teams.id
+GROUP BY teams.id
+ORDER BY score DESC
+`,(err,teams)=>{
 
-res.render("scoreboard",{users})
+res.render("scoreboard",{teams})
 
 })
 
