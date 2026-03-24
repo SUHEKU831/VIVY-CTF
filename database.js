62b546fa44e3b25e301c30946523fda74eb940b8
SUHEKU831 <suheku83@gmail.com>
2026-03-25 07:10:06 +0800

    Update database.js

--- database.js
+++ database.js
@@ -3,12 +3,10 @@ const sqlite3 = require("sqlite3").verbose()
 const bcrypt = require("bcrypt")
 
 const dbPath = path.join(__dirname,"database.db")
-
 const db = new sqlite3.Database(dbPath)
 
 db.serialize(()=>{
 
-/* USERS TABLE */
 db.run(`
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY,
@@ -19,12 +17,8 @@ isAdmin INTEGER DEFAULT 0
 )
 `)
 
-/* ✅ TAMBAH KOLOM TEAM KE USERS */
-db.run(`
-ALTER TABLE users ADD COLUMN team_id INTEGER
-`, (err)=>{})
+db.run(`ALTER TABLE users ADD COLUMN team_id INTEGER`,()=>{})
 
-/* ✅ TEAM TABLE */
 db.run(`
 CREATE TABLE IF NOT EXISTS teams(
 id INTEGER PRIMARY KEY,
@@ -32,7 +26,6 @@ name TEXT UNIQUE
 )
 `)
 
-/* CHALLENGE TABLE */
 db.run(`
 CREATE TABLE IF NOT EXISTS challenges(
 id INTEGER PRIMARY KEY,
@@ -41,11 +34,11 @@ description TEXT,
 flag_hash TEXT,
 points INTEGER,
 category TEXT,
-file TEXT
+file TEXT,
+link TEXT
 )
 `)
 
-/* SOLVES TABLE */
 db.run(`
 CREATE TABLE IF NOT EXISTS solves(
 id INTEGER PRIMARY KEY,
@@ -54,30 +47,13 @@ challenge_id INTEGER
 )
 `)
 
-db.run(`
-ALTER TABLE challenges ADD COLUMN link TEXT
-`, (err)=>{})
+db.run(`ALTER TABLE solves ADD COLUMN team_id INTEGER`,()=>{})
 
-/* ✅ TAMBAH TEAM_ID KE SOLVES */
-db.run(`
-ALTER TABLE solves ADD COLUMN team_id INTEGER
-`, (err)=>{})
-
-/* AUTO CREATE ADMIN */
 db.get("SELECT * FROM users WHERE username='admin'", async (err,row)=>{
-
 if(!row){
-
 const hash = await bcrypt.hash("Elang910",10)
-
-db.run(`
-INSERT INTO users(username,password,isAdmin)
-VALUES(?,?,1)
-`,["admin",hash])
-
-console.log("Admin account created")
+db.run(`INSERT INTO users(username,password,isAdmin) VALUES(?,?,1)`,["admin",hash])
 }
-
 })
 
 })
